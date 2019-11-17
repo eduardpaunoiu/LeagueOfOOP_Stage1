7a56a53731fae22c9fcd35d4f398216be2e2781a
Ionuț-Eduard Păunoiu <edward.paunoiu@gmail.com>
2019-11-17 17:40:42 +0200

    Am creat un fisier .gitignore care nu imi va pune pe git fisiere create de IDE, xml-uri, iml-uri, etc.

---
+++ src/com/project1/main/Main.java
@@ -0,0 +1,10 @@
+package com.project1.main;
+
+public final class Main {
+    private Main() {
+        // just to trick checkstyle
+    }
+    public static void main(final String[] args) {
+        System.out.println("Project 1");
+    }
+}
